b6b0a6208277280c00fbeaddb74a7166c265fc7f
EnzoSani <enzosanabria23@gmail.com>
2022-05-30 05:47:20 -0300

    Add MenuItem to UnitOfWork

---
+++ Abby.DataAccess/Repository/MenuItemRepository.cs
@@ -0,0 +1,38 @@
+﻿using Abby.DataAccess.Data;
+using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abby.DataAccess.Repository
+{
+    public class MenuItemRepository : Repository<MenuItem>, IMenuItemRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public MenuItemRepository(ApplicationDbContext db) : base(db)
+        {
+                _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(MenuItem obj)
+        {
+            var objFromDb = _db.MenuItem.FirstOrDefault(u => u.Id == obj.Id);
+            objFromDb.Description = obj.Description;
+            objFromDb.Price = obj.Price;
+            objFromDb.CategoryId = obj.CategoryId;
+            objFromDb.FoodTypeId = obj.FoodTypeId;
+            if(objFromDb.Image != null)
+            {
+                objFromDb.Image = obj.Image; 
+            }
+        }
+    }
+}
