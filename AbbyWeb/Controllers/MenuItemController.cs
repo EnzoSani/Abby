aca3ec3aa11e3e23434c16ea0822b0b3f5a7a24c
EnzoSani <enzosanabria23@gmail.com>
2022-06-07 05:26:46 -0300

    Implement Http Delete for Menu Item

--- AbbyWeb/Controllers/MenuItemController.cs
+++ AbbyWeb/Controllers/MenuItemController.cs
@@ -8,9 +8,12 @@ namespace AbbyWeb.Controllers
     public class MenuItemController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public MenuItemController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public MenuItemController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
 
         [HttpGet]
@@ -18,6 +21,21 @@ namespace AbbyWeb.Controllers
         {
             var menuItemList = _unitOfWork.MenuItem.GetAll(includeProperties: "Category,FoodType");
             return Json(new { data = menuItemList});
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id);
+            var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, objFromDb.Image.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.MenuItem.Remove(objFromDb);
+            _unitOfWork.Save();
+            var menuItemList = _unitOfWork.MenuItem.GetAll(includeProperties: "Category,FoodType");
+            return Json(new { succes = true, message = "Delete succesful." });
         }
     }
 }
