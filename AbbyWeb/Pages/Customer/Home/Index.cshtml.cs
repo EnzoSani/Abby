80b58cae5ada33aadc0229745b85c070348dcc63
EnzoSani <enzosanabria23@gmail.com>
2022-06-08 04:59:05 -0300

    Display Home Page

--- AbbyWeb/Pages/Customer/Home/Index.cshtml.cs
+++ AbbyWeb/Pages/Customer/Home/Index.cshtml.cs
@@ -18,7 +18,7 @@ namespace AbbyWeb.Pages.Customer.Home
         public void OnGet()
         {
             MenuItemList = _unitOfWork.MenuItem.GetAll(includeProperties: "Category,FoodType");
-            CategoryList = _unitOfWork.MenuItem.GetAll();
+            CategoryList = _unitOfWork.Category.GetAll();
         }
     }
 }
